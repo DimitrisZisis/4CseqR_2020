e91ab8828cd46f0518773b42ecd17beb336bfa6a
Dimitrios Zisis <dzis@igr.poznan.pl>
2020-11-02 13:19:07 +0200

    Create bowt_build.sh

---
+++ mm10/mapping/bowt_build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#PBS -l nodes=1:ppn=1
+#PBS -l mem=2GB
+#PBS -l walltime=00:20:00
+#PBS -N 4CSEQbuild
+
+#BT=/home/users/dzisis/bedtools-2.17.0/bin/bedtools
+
+DATA_PATH="/home/users/dzisis/4CseqR_new2018/arabidopsis/mapping"
+cd $DATA_PATH
+
+module load bowtie2
+
+bowtie2-build --offrate 1 mm10_AAGCTT_fragments.fa  mm10_AAGCTT_fragments
