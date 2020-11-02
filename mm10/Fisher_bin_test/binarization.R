bb140753e62f42d933e414014f86ce1fed53db95
Dimitrios Zisis <dzis@igr.poznan.pl>
2020-11-02 13:18:01 +0200

    Create binarization.R

---
+++ mm10/Fisher_bin_test/binarization.R
@@ -0,0 +1,8 @@
+newdata <- read.csv(file="/4CseqR-master/4CseqR/Library_Fragments/ESC_pcdhb19_1_salmon.csv", header=T, sep="\t")
+
+newdata$NumReads
+bin_param <-1
+newdata$binary <- ifelse(newdata$NumReads>bin_param, 1, 0)
+newdata[1:10,]
+write.table(newdata, file = "ESC_pcdhb19_1_salmon_binary_p_1.txt", append=TRUE , quote = FALSE,  sep = "\t", row.names=FALSE , col.names=FALSE)
+
